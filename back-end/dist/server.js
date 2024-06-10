c2be8d695b6b5307828fe60828063f9017c3d34a
Alysson Rafael <alyssonrafael11@gmail.com>
2024-06-10 10:25:27 -0300

    start deploying

--- back-end/dist/server.js
+++
@@ -1,9 +0,0 @@
-// Importa a aplicação Express configurada no módulo 'app'
-import app from './app.js';
-// Carrega as variáveis de ambiente do arquivo '.env'
-import dotenv from 'dotenv';
-dotenv.config();
-// Define a porta na qual o servidor será executado
-const PORT = process.env.PORT || 3333;
-// Inicia o servidor e imprime uma mensagem indicando a porta em que está sendo executado
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
